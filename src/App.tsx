1890545d6d7a478e0e17a542c3c64bf261cead1a
justizha <alfathizhaaaaaaa@gmail.com>
2025-06-30 10:58:59 +0700

    reponsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ export default function App() {
     <main className="min-h-screen font-mono">
       <section className="hero pt-12">
         <div className="hero-content">
-          <div className="max-w-lg">
+          <div className="sm:max-w-lg max-w-md">
             <h1 className="sm:text-5xl text-4xl font-black">Hi there!</h1>
             <p className="py-5 sm:text-lg text-[16px]">
-               I'm Izha, and I'm so glad you stopped by my little corner of the internet. Want to get in touch? Just hit that contact button!
+              I'm Izha, and I'm so glad you stopped by my little corner of the internet. Want to get in touch? Just hit that contact button!
             </p>
           </div>
         </div>
