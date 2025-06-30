306d1ff90d1a4426af79a4acc352ac933e295fa7
justizha <alfathizhaaaaaaa@gmail.com>
2025-06-30 10:58:26 +0700

    links

---
+++ src/components/Contanct.tsx
@@ -0,0 +1,67 @@
+export default function Contact() {
+ return (
+  <>
+   <button
+    onClick={() => {
+     const modal = document.getElementById('mdl') as HTMLDialogElement
+     if (modal) modal.showModal();
+    }}
+    className="btn btn-outline btn-accent w-lg">
+    <p className="text-lg">Contact</p>
+   </button>
+   {/* dialog */}
+   <dialog id="mdl" className="modal">
+    <div className="modal-box">
+
+     <form method="dialog">
+      <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+     </form>
+
+     <h3 className="font-bold text-lg pb-2 text-center">Socials & Contacts</h3>
+
+     <article>
+      <ul className="space-y-2">
+       {/* email */}
+       <li>
+        <a
+         href={'mailto:alfathizhaaaaaaa@gmail.com'}
+         rel="noopener noreferrer"
+         className="btn bg-gray-200 text-base-300  w-full text-[16px]">
+         <img src="/assets/gmail.svg" alt="gmail" className="w-5" />
+         Email
+        </a>
+       </li>
+       {/* end email */}
+
+       {/* github  */}
+       <li>
+        <a
+         href={'https://github.com/justizha'}
+         rel="noopener noreferrer"
+         target="_blank"
+         className="btn w-full btn-soft text-[16px]">
+         <img src="/assets/github.svg" alt="gmail" className="w-6" />
+         Github
+        </a>
+       </li>
+       {/* end github */}
+
+       {/* spotify  */}
+       <li>
+        <a
+         href={'https://open.spotify.com/user/31sg4u2iwexudonkopwhqckghd3u?si=0777b9ac7dc74002'}
+         rel="noopener noreferrer"
+         target="_blank"
+         className="btn w-full btn-success text-[16px]">
+         <img src="/assets/spotify.svg" alt="gmail" className="w-6" />
+         Spotify
+        </a>
+       </li>
+       {/* end spotify */}
+      </ul>
+
+     </article>
+    </div>
+   </dialog>
+  </>)
+}
--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,32 +1,10 @@
+import Contact from "./Contanct"
+
 
 export default function MainContent() {
  return (
   <section className="flex justify-center px-4 sm:px-0">
-   <button
-    onClick={() => {
-     const modal = document.getElementById('mdl') as HTMLDialogElement
-     if (modal) modal.showModal();
-    }}
-    className="btn btn-outline btn-accent w-lg">
-    <p className="text-lg">Contact</p>
-   </button>
-   <dialog id="mdl" className="modal">
-    <div className="modal-box">
-     <form method="dialog">
-      <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-     </form>
-     <h3 className="font-bold text-lg pb-2 text-center">Socials & Contacts</h3>
-     <article>
-      <a
-       href={'mailto:alfathizhaaaaaaa@gmail.com'}
-       target="_blank"
-       className="btn w-full btn-soft  text-[16px]">
-       <img src="/assets/gmail.svg" alt="gmail" className="w-5" />
-       Email
-      </a>
-     </article>
-    </div>
-   </dialog>
+   <Contact />
   </section>
  )
 }
