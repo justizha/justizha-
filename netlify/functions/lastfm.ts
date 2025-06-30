0e9516c5e1b4de8b2fe166ef80404d9e4e9fa5d3
justizha <alfathizhaaaaaaa@gmail.com>
2025-06-30 14:27:09 +0700

    env

--- netlify/functions/lastfm.ts
+++ netlify/functions/lastfm.ts
@@ -1,7 +1,7 @@
 import type { Handler } from '@netlify/functions';
 import fetch from 'node-fetch';
 
-const API_KEY = process.env.LASTFM_API_KEY;
+const API_KEY = process.env.VITE_LASTFM_API_KEY;
 const BASE_URL = 'https://ws.audioscrobbler.com/2.0/';
 
 export const handler: Handler = async (event) => {
