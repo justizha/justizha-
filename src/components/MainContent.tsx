125787b7b45930c012e3dc8fc9608536f71de345
justizha <alfathizhaaaaaaa@gmail.com>
2025-06-30 14:45:43 +0700

    dev for netlify

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,10 +1,11 @@
 import Contact from "./Contanct"
-
+import { Link } from "react-router"
 
 export default function MainContent() {
  return (
-  <section className="flex justify-center px-4">
+  <section className="grid place-items-center w-full gap-4">
    <Contact />
+   <Link className="btn btn-outline btn-warning sm:w-lg w-full text-lg" to={'/music'}>Music</Link>
   </section>
  )
 }
