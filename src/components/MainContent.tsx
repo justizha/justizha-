80af3370201fcd0a309eed060d21c3686f065933
justizha <alfathizhaaaaaaa@gmail.com>
2025-07-11 23:09:00 +0700

    fix : pading adjustment

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -4,7 +4,7 @@ import Contact from "./Contanct";
 
 export default function MainContent() {
   return (
-    <section className="grid place-items-center px-8 gap-3 pb-4">
+    <section className="grid place-items-center px-16 gap-3 pb-4">
       <Contact />
       <Link
         className="btn btn-outline btn-warning sm:w-lg w-full text-lg"
