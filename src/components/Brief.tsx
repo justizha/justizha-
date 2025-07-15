46ac8f377d85a832bd9ce9c7f83db509f7c5b1d9
justizha <alfathizhaaaaaaa@gmail.com>
2025-07-15 13:09:23 +0700

    revert

--- src/components/Brief.tsx
+++ src/components/Brief.tsx
@@ -2,7 +2,7 @@ export default function Brief() {
   return (
     <section className="pt-24">
       <div className="relative">
-        <picture>
+        {/* <picture>
           <source srcSet="/assets/zzz.avif" type="image/avif" />
           <source srcSet="/assets/zzz.webp" type="image/webp" />
           <img
@@ -11,7 +11,13 @@ export default function Brief() {
             alt="cat who is sleeping"
             className="w-40 absolute -right-5.5 -top-25 z-20"
           />
-        </picture>
+        </picture> */}
+        <img
+          loading="lazy"
+          src="/assets/zzz.gif"
+          alt="cat who is sleeping"
+          className="w-40 absolute -right-5.5 -top-25 z-20"
+        />
         <div className="rounded-md bg-base-200 sm:w-lg px-4  py-5">
           <p className="p-5 text-lg text-center">
             " I code one line at the time "
