1c3b506f5c25474857394688c684717465123a3c
justizha <alfathizhaaaaaaa@gmail.com>
2025-07-20 15:40:23 +0700

    chore : height and gif changes

--- src/components/Brief.tsx
+++ src/components/Brief.tsx
@@ -1,27 +1,17 @@
 export default function Brief() {
   return (
-    <section className="pt-24">
+    <section className="pt-5">
       <div className="relative">
-        {/* <picture>
-          <source srcSet="/assets/zzz.avif" type="image/avif" />
-          <source srcSet="/assets/zzz.webp" type="image/webp" />
-          <img
-            loading="lazy"
-            src="/assets/zzz.gif"
-            alt="cat who is sleeping"
-            className="w-40 absolute -right-5.5 -top-25 z-20"
-          />
-        </picture> */}
-        <img
-          loading="lazy"
-          src="/assets/zzz.gif"
-          alt="cat who is sleeping"
-          className="w-40 absolute -right-5.5 -top-25"
-        />
         <div className="rounded-md bg-base-200 sm:w-lg px-4  py-5">
           <p className="p-5 text-lg text-center">
-            " I code one line at the time "
+            "writing code one line at the time"
           </p>
+          <img
+            loading="lazy"
+            src="/assets/catsleep.gif"
+            alt="cat who is sleeping"
+            className="w-14  absolute  -right-4 top-19"
+          />
         </div>
       </div>
     </section>
